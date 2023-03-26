39fe5ca34e03277fa577f113d9ae3a13b5fd320f
MD. Mehrab Hossain <38918084+jansir17@users.noreply.github.com>
2023-03-27 00:51:27 +0600

    Radio Box Assertion

--- cypress/e2e/Registration.cy.js
+++ cypress/e2e/Registration.cy.js
@@ -1,14 +1,12 @@
 describe("TestSuit01", () => {
   it("Test01", () => {
-    cy.visit("https://training.nop-station.com/register?returnUrl=%2F");
-    cy.xpath("//strong[normalize-space()='Your Personal Details']").contains(
-      "Your Personal Details"
-    );
-    // cy.get(".ico-register").click();
-    // cy.get(".page-title").contains("Register");
-  });
-
-  it("Test02", () => {
-    cy.get("#FirstName").type("mehrab");
+    cy.visit("https://training.nop-station.com/");
+    cy.get(".ico-register").click();
+    cy.get(".page-title").contains("Register");
+    cy.xpath("//input[@id='gender-male']").should("be.visible");
+    cy.xpath("//input[@id='gender-female']").should("be.visible");
+    cy.xpath("//input[@id='gender-male']").check().should("be.checked");
+    cy.xpath("//input[@id='gender-female']").should("be.checked");
+    cy.xpath("(//input[@id='FirstName'])[1]").type("Mehrab");
   });
 });
